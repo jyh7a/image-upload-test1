c6fb53f39b11144039dfb1eacf76230294610f7c
jyh7a <jyh7a@naver.com>
2023-09-02 14:13:34 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const utils = require("util");
+const unlinkFile = utils.promisify(fs.unlink);
+
+const app = express();
+
+const upload = multer({ dest: "uploads" });
+
+const { uploadFile, getFileStream } = require("./s3");
+
+app.get("/images/:key", (req, res) => {
+  const key = req.params.key;
+  const readStream = getFileStream(key);
+
+  readStream.pipe(res);
+});
+
+app.post("/images", upload.single("image"), async (req, res) => {
+  const file = req.file;
+  console.log({ file });
+
+  // apply filter
+  // resize
+
+  const result = await uploadFile(file);
+  console.log({ result });
+
+  await unlinkFile(file.path);
+
+  const description = req.body.description;
+  console.log({ description });
+
+  res.send({ imagePath: `/images/${result.Key}` });
+});
+
+app.listen(8080, () => console.log(`http://127.0.0.1:8080`));
